7a741eeeef06214f9e3a11039ee8e5aba6a82749
Garmoska <leonov.eugene@gmail.com>
2024-11-09 16:01:58 +0100

    Week 01

--- week01/fizzbuzz.js
+++ week01/fizzbuzz.js
@@ -1,7 +1,7 @@
 // print numbers from 1 to 100
-// if a number divides on 3 without reminder (0) print "fizz"
-// if a number divides on 5 without reminder (0) print "buzz"
-// if a number divides on 3 AND 5 without reminder (0) print "fizzbuzz"
+// if number divides on 3 without reminder (0) print "fizz"
+// if number divides on 5 without reminder (0) print "buzz"
+// if number divides on 3 AND 5 without reminder (0) print "fizzbuzz"
 
 // 1 : "1"
 // ...
@@ -9,3 +9,19 @@
 // ...
 // 15 : "15 fizzbuzz"
 
+// hint: / -- divide ; % -- returns just a reminder
+// hint: use % in your HW 2
+
+// console.log(5 / 2); //result is 2.5
+// console.log(5 % 2); //result is 1; 5 divide on 2 = 2 reminder 1
+
+for (let i = 1; i <= 100; i++) {
+    let s = i.toString() + " : ";
+    if (i % 3 === 0){
+        s = s + "fizz";
+    }
+    if (i % 5 === 0){
+        s = s + "buzz";
+    }
+    console.log(s);
+}
